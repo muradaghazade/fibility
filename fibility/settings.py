a92c0e6e781445783adb905c4435a444b01d2481
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2021-03-03 00:25:18 +0400

    changes

--- fibility/settings.py
+++ fibility/settings.py
@@ -143,8 +143,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'xomurad41@gmail.com'
-EMAIL_HOST_PASSWORD = 'babanene'
-EMAIL_PORT = 587
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'husubayli@gmail.com'
+EMAIL_HOST_PASSWORD = 'xdjnasiuddxikfax'
